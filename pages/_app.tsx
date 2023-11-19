7da87e0f54fae780f78090eaf56b364cb3ea1e8e
gmap-nitro <geraldo.filho92@hotmail.com>
2023-11-19 09:49:50 -0300

    feat: add and config component lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
-import { HelloWorld } from "../src/components/HelloWorld";
-import "../public/css/styles.css";
-import React from "react";
+import { StyleProvider, ThemePicker } from 'vcc-ui'
+import { HelloWorld } from '../src/components/HelloWorld'
+import '../public/css/styles.css'
+import React from 'react'
 
 function HomePage() {
   return (
     <React.StrictMode>
-      <HelloWorld />
+      <StyleProvider>
+        <ThemePicker variant='ligth'>
+          <HelloWorld />
+        </ThemePicker>
+      </StyleProvider>
     </React.StrictMode>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
